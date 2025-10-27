6f76e1161cd65dbd28f32b0d7a68e0fc507a7e4e
darilrosales88 <daril.rosales88@gmail.com>
2025-10-27 11:40:32 -0500

    creacion de componente de calculo

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
-<script setup></script>
+<script setup>
+import { ref} from 'vue'; //para la utilizacion de variables reactivas
+
+const contador =  ref(0) //iniciando una variable reactiva en valor 0
+const incrementar = () => {
+  contador.value++; //contador.value es la manera de acceder al valor de una variable reactiva
+}
+
+const decrementar = () => {
+  contador.value--; //contador.value es la manera de acceder al valor de una variable reactiva
+}
+
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>Estas aquí, este es el inicio!</h1>
+  <p> la variable contador tiene valor {{contador}}</p>
+  <p><button @click="incrementar">incrementar</button></p>
+  <p><button @click="decrementar">decrementar</button></p> 
 </template>
 
-<style scoped></style>
+<style scoped>
+p{
+  color: red;
+}
+</style>
---
+++ src/components/contador.vue
@@ -0,0 +1,11 @@
+<script setup>
+import contador from '.components/contador.vue' //importando al componente nuevo creado
+</script>
+
+<template>
+ <contador></contador> <!-- llamando al componente nuevo creado-->
+</template> 
+
+<style scoped>
+
+</style>
